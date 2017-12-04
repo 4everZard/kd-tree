a1afcddf033c7a2ea90e0373c315e24fe3ce6c1f
developer <developer@dev-vbox>
2017-12-04 13:47:34 -0500

    first commit

---
+++ kd-tree/src/PointSET.java
@@ -0,0 +1,48 @@
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Set;
+public class PointSET {
+	private Set<Point2D> points;
+	public PointSET() {
+		points = new SET<Point2D>();
+	}
+	public boolean isEmpty() {
+		return points.isEmpty();
+	}
+	public int size() {
+		return points.size();
+	}
+	public void insert(Point2D p) {
+		points.add(p);
+	}
+	public boolean contains(Point2D p) {
+		return points.contains(p);
+	}
+	public void draw() {
+		for(Point2D p: points) {
+			p.draw();
+		}
+	}
+	public Iterable<Point2D> range(RectHV rect){
+		ArrayList<Point2D> inRange = new ArrayList<Point2D>();
+		for(Point2D p: points) {
+			if(rect.contains(p)) {
+				inRange.add(p)				
+			}		
+		}
+		return inRange;
+	}
+	public Point2D nearest(Point2D p) {
+		Point2D minPoint = null;
+		double distance =0.0;
+		double minDistance = Double.POSITIVE_INFINITY;
+		for(Point2D point: points) {
+			distance = p.distanceTo(point);
+			if(distance < minDistance) {
+				minPoint = point;
+				minDistance = distance;
+			}
+		}
+		return minPoint;
+	}
+}
