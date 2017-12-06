a5aaa5d4e30ea268c122efa0d85bd2a3fdb0a417
developer <developer@dev-vbox>
2017-12-06 16:30:14 -0500

    second commit

--- kd-tree/src/KdTree.java
+++ kd-tree/src/KdTree.java
@@ -1,4 +1,295 @@
+import java.util.Comparator;
+import java.util.ArrayList;
 
 public class KdTree {
+	// constructor
+	private Node root;
+	private int size;
+	
+	public KdTree() {
+		root = null;
+		size = 0;
+	}
+	
+	
+	public int size() {
+		return size;
+	}
+	
+	//create private static node class
+	private static class Node{
+		private Point2D point;
+		private RectHV rect;
+		private boolean isVertical;
+		private Node left;
+		private Node right;
+		
+		private Node(Point2D p, boolean vertical, Node left, Node right, RectHV rectangle) {
+			this.point = p;
+			this.isVertical = vertical;
+			this.left = left;
+			this.right = right;
+			this.rect = rectangle;
+		}
+		
+		public RectHV getRect() {
+			return this.rect;
+		}
+		public Point2D getPoint() {
+			return point;
+		}
+		public boolean vertical() {
+			return isVertical;
+		}
+		public Node getLeft() {
+			return left;
+		}
+		public Node getRight() {
+			return right;
+		}
+		public void setLeft(Node leftNode) {
+			this.left = leftNode;
+		}
+		public void setRightNode(Node rightNode) {
+			this.right = rightNode;
+		}
+	}
+	
+	// add the point to the set
+	public void insert(Point2D p) {
+		// point current node to root
+		Node currNode = root;
+		Comparator<Point2D> comparator = Point2D.Y_ORDER;
+		// if the tree is empty
+		if(size == 0) {
+			RectHV rect = new RectHV(0.0, 0.0, 1.0, 1.0);
+			root = new Node(p, true, null, null, rect);
+			size++;
+			return;
+		}
+		
+		while(currNode != null) {
+			// return if inserted point is already in a tree
+			if(currNode.getPoint().equals(p))
+				return;
+			// if current node is vertical, set comparator to x order
+			if(currNode.vertical()) {
+				comparator = Point2D.X_ORDER;
+			}
+			else {
+				comparator = Point2D.Y_ORDER;
+			}
+			
+			//
+			if(comparator.compare(p, currNode.getPoint()) < 0) {
+				if(currNode.getLeft() == null){
+					RectHV rect = null;
+					// if current node is vertical type, point to left rectangle, else point to bottom
+					if(currNode.vertical()) {
+						rect = new RectHV(currNode.getRect().xmin(), currNode.getRect().ymin(), currNode.getPoint().x(), currNode.getRect().ymax())
+					}
+					else {
+						rect = new RectHV(currNode.getRect().xmin(), currNode.getRect().ymin(), currNode.getRect().xmax(), currNode.getPoint().y());
+					}
+					
+					// create new left node and insert the point p into it
+					Node leftNode = new Node(p, !currNode.vertical(), null, null, rect);
+					//point the left node of current node to new left node
+					currNode.setLeft(leftNode);
+					size++;
+					break;	
+				}
+				// set current node to new left node
+				currNode = currNode.getLeft();
+			}
+			else {
+				if(currNode.getRight() == null) {
+					RectHV rect = null;
+					// if current node is vertical type, point to right rectangle, else point to top
+					if(currNode.vertical()) {
+						rect = new RectHV(currNode.getPoint().x(), currNode.getRect().ymin(), currNode.getRect().xmax(), currNode.getRect().ymax());
+					}
+					else {
+						rect = new RectHV(currNode.getRect().xmin(), currNode.getPoint().y(), currNode.getRect().xmax(), currNode.getRect().ymax());
+					}
+					Node rightNode = new Node(p, !currNode.vertical(), null, null, rect);
+					currNode.setRightNode(rightNode);
+					size++;
+					break;
+				}
+				currNode = currNode.getRight();
+				
+			}
+		}	
+	}
+	
+	
+	// does the set contains point p?
+	public boolean contains(Point2D p) {
+		return containsRecursive(root,p);
+	}
+	
+	private boolean containsRecursive(Node currNode, Point2D targetPoint) {
+		if (currNode == null) {
+			return false;
+		}
+		Point2D currPoint = currNode.getPoint();
+		if(currPoint.equals(targetPoint)) {
+			return true;
+		}
+		
+		Comparator<Point2D> comparator = null;
+		if(currNode.vertical()) {
+			comparator = Point2D.X_ORDER;
+		}
+		else {
+			comparator = Point2D.Y_ORDER;
+		}
+		if(comparator.compare(targetPoint, currPoint) < 0) {
+			return containsRecursive(currNode.getLeft(), targetPoint);
+		}
+		else {
+	        return containsRecursive(currNode.getRight(), targetPoint);
+	    }	
+	}
+	
+	public void draw() {
+	    StdDraw.setPenColor(StdDraw.BLACK);
+	    Point2D bottomMin = new Point2D(0.0, 0.0);
+	    Point2D bottomMax = new Point2D(1.0, 0.0);
+	    bottomMin.drawTo(bottomMax);
+	    Point2D leftMin = new Point2D(0.0, 0.0);
+	    Point2D leftMax= new Point2D(0.0, 1.0);
+	    leftMin.drawTo(leftMax);
+	    Point2D topMin = new Point2D(0.0, 1.0);
+	    Point2D topMax = new Point2D(1.0, 1.0);
+	    topMin.drawTo(topMax);
+	    Point2D rightMin = new Point2D(1.0, 0.0);
+	    Point2D rightMax = new Point2D(1.0, 1.0);
+	    rightMin.drawTo(rightMax);
+	    drawRecursive(root);
+	}
+	
+	private void drawRecursive(Node node) {
+	    if (node == null) {
+	        return;
+	    }
+	    RectHV rect = node.getRect();
+	    Point2D point = node.getPoint();
+	    
+	    StdDraw.setPenColor(StdDraw.BLACK);
+	    StdDraw.setPenRadius(0.01);
+	    point.draw();
+	    
+	    if (node.vertical()) {
+	        StdDraw.setPenColor(StdDraw.RED);
+	        StdDraw.setPenRadius();
+	        
+	        Point2D bottom = new Point2D(point.x(), rect.ymin());
+	        Point2D top = new Point2D(point.x(), rect.ymax());
+	        bottom.drawTo(top);
+	        
+	        drawRecursive(node.getLeft());
+	        drawRecursive(node.getRight());
+	    } 
+	    else
+	    {
+	        StdDraw.setPenColor(StdDraw.BLUE);
+	        StdDraw.setPenRadius();
+	        
+	        Point2D left = new Point2D(rect.xmin(), point.y());
+	        Point2D right = new Point2D(rect.xmax(), point.y());
+	        left.drawTo(right);
+	        
+	        drawRecursive(node.getLeft());
+	        drawRecursive(node.getRight());
+	    }
+	}
+	
+	// start at the root and recursively search for points in both subtrees to find all points contained in a give rectangle
+	public Iterable<Point2D> range(RectHV rect){
+		ArrayList<Point2D> pointsInRange = new ArrayList<Point2D>();
+		rangeRecursive(pointsInRange, rect, root);
+		return pointsInRange;
+	}
+	
+	private void rangeRecursive(ArrayList<Point2D> rangeList, RectHV rect, Node currNode) {
+		if(currNode == null)
+			return;
+		Point2D currPoint = currNode.getPoint();
+		if(rect.contains(currPoint))
+			rangeList.add(currPoint);
+		double pointCoord = currPoint.x();
+		double rectMin = rect.ymin();
+		double rectMax = rect.xmin();
+		if(!currNode.vertical()) {
+		     pointCoord = currPoint.x();
+             rectMin = rect.xmin();
+             rectMax = rect.xmax();
+		}
+		
+	    if (pointCoord > rectMin)
+	    	rangeRecursive(rangeList, rect, currNode.getLeft());
+	    if (pointCoord <= rectMax)
+	    	rangeRecursive(rangeList, rect, currNode.getRight());
+		
+	}
+	
+	private Comparator<Point2D> distanceOrder(Point2D targetPoint){
+		return new Comparator<Point2D>(){
+			public int compare(Point2D p1, Point2D p2) {
+				double distance1 = targetPoint.distanceSquaredTo(p1);
+				double distance2 = targetPoint.distanceSquaredTo(p2);
+				if(distance1 < distance2) return -1;
+				else if (distance1 > distance2) return 1;
+				else return 0;
+			}
+			
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
